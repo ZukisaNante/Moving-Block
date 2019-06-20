0569493f5683b385f974462ac46582c241ccc014
junior <zukisanante@gmail.com>
2019-06-20 16:55:57 +0200

    js/main.js

--- js/main.js
+++ js/main.js
@@ -1,170 +1,56 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//declaring  variables
+var keys = {}; //storing the codes and current used/pressed keys
+keys.UP = 38;
+keys.DOWN = 40;
+keys.LEFT = 37;
+keys.RIGHT = 39;
+
+//Store character reference
+var foodHunter = {
+    x: 100,
+    y: 100,
+    speedMultiply: 2,
+    element: document.getElementById('foodHunter'),
+};
+
+//key detection - try it after with eventListener
+document.body.onkeyup = document.body.onkeydown = function(e) {
+    if (e.preventDefault) {
+        e.preventDefault();
+    } else {
+        e.returnValue = false;
+    }
+    var kc = e.keyCode || e.which;
+    keys[kc] = e.type == 'keydown';
+};
+/// character movement update
+var moveCharacter = function(dx, dy) {
+    character.x += (dx || 0) * character.speedMultiplier;
+    character.y += (dy || 0) * character.speedMultiplier;
+    character.element.style.left = character.x + 'px';
+    character.element.style.top = character.y + 'px';
+};
+
+/// character control
+var detectCharacterMovement = function() {
+    if (keys[keys.LEFT]) {
+        moveCharacter(-1, 0);
+    }
+    if (keys[keys.RIGHT]) {
+        moveCharacter(1, 0);
+    }
+    if (keys[keys.UP]) {
+        moveCharacter(0, -1);
+    }
+    if (keys[keys.DOWN]) {
+        moveCharacter(0, 1);
+    }
+};
+
+/// update current position on screen
+moveCharacter();
+
+/// game loop
+setInterval(function() {
+    detectCharacterMovement();
+}, 1000 / 24);
